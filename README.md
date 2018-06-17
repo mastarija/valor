7ad8a130de786951e6bb79d905046f9986e67073
Luka Hadžiegrić <reygoch@gmail.com>
2018-06-17 11:09:33 +0200

    working on motivation

--- README.md
+++ README.md
@@ -9,60 +9,38 @@
 
 # Valor
 
-Simple, minimal and easy to use Haskell validation library that gives you the
-full control over how you handle your data types.
+General, simple and easy to use structured validation library that gives you
+control over the error and input types.
 
 ## Motivation
 
-There are a few validation libraries out there, most notably [forma][1]
-and [digestive-functors][3]. The problem I have with those is that they are all
-either too speciffic or too complicated for my taste.
+Currently there are a few validation libraries out there, most notably
+[forma][1] and [digestive-functors][2]. They are acceptable but they are mostly
+geard towards the web. Even with that in mind, I find them a bit impractical to
+use so I've decided to make a validation library of my own.
 
-`digestive-functor`s use custom types to validate form and they even try to
-handle the view generation. They are also more suited for `HTML` form validation
-and rendering.
+Here are the two main problems I have with them:
 
-`forma` on the other hand doesn't try to do the rendering for me but it plays a
-role of a `JSON` parser and forces me to use [Value][3] as the type of data that
-I want to validate.
++ They limit what you can validate and what you get as a result of that
+  validation. [Forma][1] expects a JSON `Value` as an input and gives you either
+  parsed data as a result or an error in the form of a JSON `Value`.
 
-This might be acceptable if you are just doing some web development but if you
-are doing some analysis and want to find out how much of the data fits your
-requirements and than reformat it based on resulting errors than it can become
-annoying to marshal you data to and from `JSON` format.
+  Additionally, [digestive-functors][2] use a custom result type that you need
+  to get familiar with.
 
-I'm also not satisfied how this looks in combination with [servant][2]. Usually
-we would do something like this:
++ They are essentially parsers, and I personally don't like to manually handle 
+  conversion of JSON fields from e.g. string to integers and other data types.
 
-```
-"user" :> "login" :> ReqBody '[JSON] UserCredentials :> Post '[JSON] Token
-```
+  Sure, it might be usefull to tell the user that he entered text instead of a
+  number, but in that case I'd argue that your submission form is bad.
 
-This way servant will receive `JSON` request and try to parse it to
-`UserCredentials` automatically and throw an error if he doesn't succeed. After
-that, we get `UserCredentials` into our `Handler` which we can than validate how
-ever we please.
+  Even in this case, it should still be possible to validate plain JSON with 
+  Valor, but if that is your usecase I'd recommend you use [forma][1] for that
+  since it was speciffically designed with JSON in mined.
 
-If we use `forma` for example, than we loose semantics that tell us what exactly
-is being posted to the server e.g.:
 
-```
-"user" :> "login" :> ReqBody '[JSON] Value :> Post '[JSON] Token
-```
-
-Sure, it might be useful to tell the user if he entered a text instead of a
-number, but than I'd argue that your form is broken if such mistake is possible,
-or if we are talking about API than sent data is invalid and should result in
-an error anyway.
-
-My opinion is that we should let parsers do the parsing and validation libraries
-do the validation.
-
-With that said, it is still possible to send JSON `Value` to the server and
-perform validation on it because Valor is agnostic about what kind of data you
-put in and expect to get out.
 
 [1]: https://hackage.haskell.org/package/forma
-[2]: https://hackage.haskell.org/package/servant
-[3]: https://hackage.haskell.org/package/digestive-functors
+[2]: https://hackage.haskell.org/package/digestive-functors
+[3]: https://hackage.haskell.org/package/servant
 [4]: https://hackage.haskell.org/package/aeson-1.4.0.0/docs/Data-Aeson.html#t:Value
--- src/Data/Valor.hs
+++ src/Data/Valor.hs
@@ -7,7 +7,10 @@
 -- Stability   :  experimental
 -- Portability :  portable
 --
+-- This module provides a general way for validating data. It was inspired by
+-- @forma@ and @digestive-functors@ and some of their shortcomings.
 --
+-- Ease of use was the main focus of this module.
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 --
@@ -20,9 +23,12 @@ module Data.Valor
   , mapcheck
   , mapchecks
   , enterField
-    -- * Validating data
+    -- * Validating the data
   , validate
   , validatePure
+    -- * Utilities
+  , Validatable
+  , Validate
   ) where
 --
 import Data.Maybe ( isJust )
@@ -34,27 +40,7 @@ import Control.Monad.Trans.Except ( ExceptT, runExceptT )
 import Data.Functor.Identity ( Identity (..) )
 --
 
---------------------------------------------------------------------------------
--- | Internal datatype used for handling users error structure.
-data Validated e = Valid e | Invalid e
-  deriving ( Show )
-
-instance Semigroup e => Semigroup (Validated e) where
-  Valid _    <> x          = x
-  x          <> Valid _    = x
-  Invalid e1 <> Invalid e2 = Invalid (e1 <> e2)
-
-instance Functor Validated where
-  fmap f (Valid   e) = Valid   $ f e
-  fmap f (Invalid e) = Invalid $ f e
-
-instance Applicative Validated where
-  pure                     = Valid
-  Valid   ef <*> Valid   e = Valid   $ ef e
-  Valid   ef <*> Invalid e = Invalid $ ef e
-  Invalid ef <*> Valid   e = Invalid $ ef e
-  Invalid ef <*> Invalid e = Invalid $ ef e
-
+{- Constructing a 'Validator' -}
 
 --------------------------------------------------------------------------------
 -- | This type represents a 'Validator' that can be run on your custom data
@@ -81,6 +67,67 @@ instance Applicative m => Applicative (Validator i m) where
 
 
 --------------------------------------------------------------------------------
+-- | This will allow you to not validate a certain field.
+skip :: Applicative m => Validator i m (Maybe e)
+skip = Validator $ \_ -> pure $ Valid Nothing
+
+
+--------------------------------------------------------------------------------
+-- | Check if a single condition is satisfied.
+check :: forall i x m e. (Functor m, Monoid e)
+  => (i -> x)                -- ^ field selector
+  -> (x -> ExceptT e m x)    -- ^ check
+  -> Validator i m (Maybe e) -- ^ resulting validator
+check sel chk = mconvert $ Validator $ unValidator (validator chk) . sel
+
+
+--------------------------------------------------------------------------------
+-- | Check if mutiple conditions are satisfied.
+checks
+  :: forall i x m e. ( Applicative m, Monoid e, Semigroup e )
+  => (i -> x)
+  -> [x -> ExceptT e m x]
+  -> Validator i m (Maybe e)
+checks sel chks = foldr1 (<>) $ fmap (check sel) chks
+
+--------------------------------------------------------------------------------
+-- | Apply a single check to multiple values within 'Traversable' structure.
+mapcheck :: forall i f x m e. (Traversable f, Foldable f, Monad m, Monoid e)
+  => (i -> f x)
+  -> (x -> ExceptT e m x)
+  -> Validator i m (Maybe (f (Maybe e)))
+mapcheck sel chk = Validator $ \i -> do
+  res <- mapM (validate $ validator chk) (sel i)
+  pure $ if all isJust res then Invalid $ Just res else Valid Nothing
+
+--------------------------------------------------------------------------------
+-- | Apply a multiple checks to values within 'Traversable' structure.
+mapchecks
+  :: forall i f x m e.
+  ( Monad m
+  , Monoid e
+  , Traversable f
+  , Semigroup (f (Maybe e))
+  )
+  => (i -> f x)
+  -> [x -> ExceptT e m x]
+  -> Validator i m (Maybe (f (Maybe e)))
+mapchecks sel chks = foldr1 (<>) $ fmap (mapcheck sel) chks
+
+
+--------------------------------------------------------------------------------
+-- | Apply a 'Validator' instead of check to the field. This is useful when
+-- validating nested records.
+enterField :: forall i x m e. (Functor m)
+  => (i -> x)
+  -> Validator x m e
+  -> Validator i m (Maybe e)
+enterField sel (Validator x) = mconvert $ Validator $ \i -> x $ sel i
+
+
+{- Validating the data -}
+
+--------------------------------------------------------------------------------
 -- | Once you have constructed your 'Validator' you can run it against your
 -- input data. If there were no validation errory you will get 'Nothing' wrapped
 -- in a monad of your choice as a result.
@@ -98,6 +145,20 @@ validate (Validator v) i = helper <$> v i
 validatePure :: Validator i Identity e -> i -> Maybe e
 validatePure v i = runIdentity $ validate v i
 
+
+{- Utilities -}
+
+--------------------------------------------------------------------------------
+-- | A simple type family used for constructing your data structure.
+type family Validatable a e x where
+  Validatable Validate e x = Maybe e
+  Validatable Identity e x = x
+  Validatable a        e x = a x
+
+--------------------------------------------------------------------------------
+-- | Tag used with type family to tell the compiler that we are constructing the
+-- "error" record.
+data Validate a
 
 --------------------------------------------------------------------------------
 -- Utility function used for converting 'Validator' error from one type to
@@ -132,65 +193,27 @@ validator chk = Validator $ \i ->
   either Invalid (const $ Valid mempty) <$> (runExceptT $ chk i)
 
 
---------------------------------------------------------------------------------
--- | This will allow you to not validate a certain field.
-skip :: Applicative m => Validator i m (Maybe e)
-skip = Validator $ \_ -> pure $ Valid Nothing
-
---
-
-check :: forall i x m e. (Functor m, Monoid e)
-  => (i -> x)
-  -> (x -> ExceptT e m x)
-  -> Validator i m (Maybe e)
-check sel chk = mconvert $ Validator $ unValidator (validator chk) . sel
-
---
-
-mapcheck :: forall i f x m e. (Traversable f, Foldable f, Monad m, Monoid e)
-  => (i -> f x)
-  -> (x -> ExceptT e m x)
-  -> Validator i m (Maybe (f (Maybe e)))
-mapcheck sel chk = Validator $ \i -> do
-  res <- mapM (validate $ validator chk) (sel i)
-  pure $ if all isJust res then Invalid $ Just res else Valid Nothing
-
---
-
-checks
-  :: forall i x m e. ( Applicative m, Monoid e, Semigroup e )
-  => (i -> x)
-  -> [x -> ExceptT e m x]
-  -> Validator i m (Maybe e)
-checks sel chks = foldr1 (<>) $ fmap (check sel) chks
-
---
-
-mapchecks
-  :: forall i f x m e.
-  ( Monad m
-  , Monoid e
-  , Traversable f
-  , Semigroup (f (Maybe e))
-  )
-  => (i -> f x)
-  -> [x -> ExceptT e m x]
-  -> Validator i m (Maybe (f (Maybe e)))
-mapchecks sel chks = foldr1 (<>) $ fmap (mapcheck sel) chks
-
---
-
-enterField :: forall i x m e. (Functor m)
-  => (i -> x)
-  -> Validator x m e
-  -> Validator i m (Maybe e)
-enterField sel (Validator x) = mconvert $ Validator $ \i -> x $ sel i
+{- Internal stuff -}
 
 --------------------------------------------------------------------------------
+-- | Internal datatype used for handling users error structure.
+data Validated e = Valid e | Invalid e
+  deriving ( Show )
 
-data Validate a
+instance Semigroup e => Semigroup (Validated e) where
+  Valid _    <> x          = x
+  x          <> Valid _    = x
+  Invalid e1 <> Invalid e2 = Invalid (e1 <> e2)
 
-type family Validatable a e x where
-  Validatable Validate e x = Maybe e
-  Validatable Identity e x = x
-  Validatable a        e x = a x
+instance Functor Validated where
+  fmap f (Valid   e) = Valid   $ f e
+  fmap f (Invalid e) = Invalid $ f e
+
+instance Applicative Validated where
+  pure                     = Valid
+  Valid   ef <*> Valid   e = Valid   $ ef e
+  Valid   ef <*> Invalid e = Invalid $ ef e
+  Invalid ef <*> Valid   e = Invalid $ ef e
+  Invalid ef <*> Invalid e = Invalid $ ef e
+
+
