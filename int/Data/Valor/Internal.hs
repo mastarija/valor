8597d98b627c885802f7b78582d8e31467e09449
Maštarija <master@mastarija.com>
2021-08-22 17:23:15 +0200

    another day, another rewrite

--- int/Data/Valor/Internal.hs
+++ int/Data/Valor/Internal.hs
@@ -1,78 +1,166 @@
 module Data.Valor.Internal where
 --
 
-{-|
-  The core internal datatype that is carrying and accumulating the errors.
+{- |
+  'Valor' (__VAL__idat__OR__) is the centerpiece of this validation library. You
+  can think of it as a function from an input to a possible error. Do check the
+  details in the "Data.Valor.Internal"
 -}
-data ValorI e
-  = Inert e
-  | Wrong e
+newtype Valor i m e = Valor
+  { unValor :: i -> m ( Wrong e )
+  }
+
+instance ( Monad m , Semigroup e ) => Semigroup ( Valor i m e ) where
+  Valor b <> Valor d = Valor $ \ i -> (<>) <$> ( b i ) <*> ( d i )
+
+instance ( Monad m , Monoid e ) => Monoid ( Valor i m e ) where
+  mempty = Valor $ const $ pure mempty
+
+instance Monad m => Functor ( Valor i m ) where
+  fmap f ( Valor v ) = Valor $ \ i -> fmap f <$> v i
+
+instance Monad m => Applicative ( Valor i m ) where
+  pure = Valor . const . pure . pure
+
+  Valor b <*> Valor d = Valor $ \ i -> (<*>) <$> ( b i ) <*> ( d i )
+
+instance Monad m => Monad ( Valor i m ) where
+  Valor v >>= evv' = Valor $ \ i -> do
+    ve <- v i
+    case ve of
+      Inert e -> unValor ( evv' e ) i
+      Wrong e -> unValor ( evv' e ) i >>= pure . Wrong . valW
 
 --
 
-{-|
-  'Inert' values are always ignored (unless there are two, then the second one
-  is returned). This is because they are essentially just a placeholder.
+{- |
+  Simple wrapper holding a 'Valid' value that has successfully passed the
+  validation. It's not supposed to be mapped over, parsed, read, coerced etc.
+  (so as to not modify / spoil the 'Valid' value).
 -}
-instance Semigroup e => Semigroup ( ValorI e ) where
-  Inert _ <> v = v
-  Wrong e <> Inert _ = Wrong e
+newtype Valid a = Valid a
+  deriving ( Eq , Show )
+
+{- |
+  Extract a value from the 'Valid' wrapper for further use / processing.
+-}
+unValid :: Valid a -> a
+unValid ( Valid a ) = a
+
+--
+
+{- |
+  The internal data type used to accumulate errors and keep track of the error
+  state (if there was an actual error or not).
+-}
+data Wrong e = Inert e | Wrong e
+
+{- |
+  'Wrong.Inert' operands are ignored and 'Wrong.Wrong' operands are 'mappend'ed.
+  If both operands are 'Inert.Inert' then the first one is ignored. If
+  'Wrong.Wrong' is one of the operands then the resulting value is also
+  'Wrong.Wrong'.
+-}
+instance Semigroup e => Semigroup ( Wrong e ) where
+  Inert _ <> x       = x
+  x       <> Inert _ = x
   Wrong b <> Wrong d = Wrong $ b <> d
 
-{-|
-  'ValorI' is a 'Monoid' only if the inner error type is a monoid.
+{- |
+  The 'Monoid's 'mempty' is implemented as @'Wrong.Inert' 'mempty'@.
 -}
-instance Monoid e => Monoid ( ValorI e ) where
+instance Monoid e => Monoid ( Wrong e ) where
   mempty = Inert mempty
 
-{-|
-  Simple and clean 'Functor' instance applying the function over the inner
-  value.
+{- |
+  Just a simple 'Functor' instance which applies the function to the value
+  within.
 -}
-instance Functor ValorI where
+instance Functor Wrong where
   fmap f ( Inert e ) = Inert $ f e
   fmap f ( Wrong e ) = Wrong $ f e
 
-{-|
-  The 'pure' is defined as 'Inert'. This is because we don't want to introduce
-  an error immediately if we just want to apply e.g. a constructor over the
-  'Applicative' values.
+{- |
+  'Applicative's 'pure' is implemented as 'Wrong.Inert'. If 'Wrong.Wrong' is
+  encountered in any of the operands then the result will also be 'Wrong'.
 -}
-instance Applicative ValorI where
+instance Applicative Wrong where
   pure = Inert
 
   Inert f <*> Inert e = Inert $ f e
   Inert f <*> Wrong e = Wrong $ f e
+
   Wrong f <*> Inert e = Wrong $ f e
   Wrong f <*> Wrong e = Wrong $ f e
 
+{- |
+  This monad instance will detect the 'Wrong.Wrong' value and propagate it to
+  the final result. Instance is not very useful since this is an internal
+  library, and exists simply because it can. :P
+-}
+instance Monad Wrong where
+  Inert e >>= ewe' = ewe' e
+  Wrong e >>= ewe' = case ewe' e of
+    Inert e' -> Wrong e'
+    Wrong e' -> Wrong e'
+
 --
 
-{-|
-  Implementation of the 'Alternative' operator without the 'empty'. If we could
-  limit the internal type of the 'Alternative' type class, there would be a
-  viable 'empty' and thus an 'Alternative' instance. Buuut... since we can't,
-  this is the best we can do without introducing more complexity.
+{- |
+  An alias for the 'mappend' ('<>').
 -}
-altI :: Semigroup e => ValorI e -> ValorI e -> ValorI e
-altI ( Inert e ) _ = Inert e
-altI _ ( Inert e ) = Inert e
-altI ( Wrong b ) ( Wrong d ) = Wrong $ b <> d
+conW :: Semigroup e => Wrong e -> Wrong e -> Wrong e
+conW = (<>)
+
+{- |
+  Non accumulating 'Alternative'. As long as there's one 'Wrong.Inert' value the
+  resulting value will be 'Inert'. However, if there are two 'Wrong's then only
+  the second one will be returned as a resulting value. If there are two
+  'Inert's then only the first one is returned.
+-}
+altW :: Wrong e -> Wrong e -> Wrong e
+altW ( Inert e ) _           = Inert e
+altW _           ( Inert e ) = Inert e
+altW ( Wrong _ ) x           = x
+
+{- |
+  Accumulating 'Alternative'. Almost the same as 'altW' except if there are two
+  'Wrong.Wrong's they are 'mappend'ed together.
+-}
+accW :: Semigroup e => Wrong e -> Wrong e -> Wrong e
+accW ( Inert e ) _           = Inert e
+accW _           ( Inert e ) = Inert e
+accW ( Wrong b ) ( Wrong d ) = Wrong $ b <> d
+
+{- |
+  Extracts the value contained within the 'Wrong' regardless if the "internal"
+  state is 'Wrong.Inert' or 'Wrong.Wrong'.
+-}
+valW :: Wrong e -> e
+valW ( Inert e ) = e
+valW ( Wrong e ) = e
+
+{- |
+  If the given value is 'Wrong.Wrong', the first function will be applied, if
+  the value is 'Wrong.Inert' then the second function will be applied.
+-}
+wrong :: ( e -> a ) -> ( e -> a ) -> Wrong e -> a
+wrong _ fi ( Inert e ) = fi e
+wrong fw _ ( Wrong e ) = fw e
+
+{- |
+  Checks if the value is 'Wrong.Inert'.
+-}
+isInert :: Wrong e -> Bool
+isInert ( Inert _ ) = True
+isInert ( Wrong _ ) = False
+
+{- |
+  Checks if the value is 'Wrong.Wrong'.
+-}
+isWrong :: Wrong e -> Bool
+isWrong ( Inert _ ) = False
+isWrong ( Wrong _ ) = True
 
 --
 
-{-|
-  Gives back the inner error value, regardless if it's 'Inert' or 'Wrong'.
--}
-valI :: ValorI e -> e
-valI ( Inert e ) = e
-valI ( Wrong e ) = e
-
-{-|
-  Checks if the 'ValorI' value is 'Inert'.
--}
-isInertI :: ValorI e -> Bool
-isInertI ( Inert _ ) = True
-isInertI ( Wrong _ ) = False
-
---
--- lib/Data/Valor.hs
+++ lib/Data/Valor.hs
@@ -1,22 +1,19 @@
-{- |
-  Simple, easy to use and powerful structured validation library that gives you
-  control over the error and input types.
--}
 module Data.Valor
   ( Valid
   , unValid
   , Valor
+  , con
   , alt
+  , acc
   , pass
-  , pass'
   , fail
-  , fail'
-  , make
   , test
+  , make
+  , peek
   , poke
-  , toM
-  , toE
-  , check
+  , nerf
+  , peer
+  , adapt
   , check1
   , checkN
   , validateP
@@ -27,262 +24,164 @@ module Data.Valor
 import Prelude hiding ( fail )
 --
 import Data.Bool ( bool )
+import Data.Valor.Internal ( Valid (..) , unValid , Valor (..) , Wrong (..) , altW , accW , valW , wrong , isInert )
 import Data.Functor.Identity ( Identity (..) )
-import Data.Valor.Internal ( ValorI (..) , altI , valI , isInertI )
 --
 
-{-|
-  A wrapper indicating that the containing value is indeed a 'Valid' one. You
-  are not supposed to be able to construct it your self. You can only obtain it
-  by successfully running a 'ValorI'' validator over a value with 'validateP',
-  'validateM' or 'toE'.
-
-  You also shouldn't be able to map over it or modify it in any way, as then it
-  could no longer be 'Valid'.
+{- |
+  An alias for 'mappend' (<>).
 -}
-newtype Valid a = Valid a
-  deriving ( Show )
+con :: ( Monad m , Semigroup e ) => Valor i m e -> Valor i m e -> Valor i m e
+con = (<>)
 
-{-|
-  Used to extract a valid value from 'Valid' for further processing.
+{- |
+  As an alternative to the 'Alternative' type class and the '<|>' operator 'alt'
+  is provided. It will result in an error only if both arguments are
+  'Wrong.Wrong', however, only the last error will be returned.
 -}
-unValid :: Valid a -> a
-unValid ( Valid a ) = a
+alt :: Monad m => Valor i m e -> Valor i m e -> Valor i m e
+alt ( Valor b ) ( Valor d ) = Valor $ \ i -> altW <$> ( b i ) <*> ( d i )
 
---
-
-{-|
-  The validator or 'ValorI' for short. The core idea of validation (according to
-  me) is taking some input and returning an error if it exists.
-
-  With that in mind, you can think of 'ValorI' as a function from some input @i@
-  to some possible error @e@. In fact, here's the exact underlying type:
-
-  @i -> m ( 'ValorI' e )@
-
-  'ValorI' is an underlying data type that carries and accumulates the errors.
-  You are not supposed to interact with it directly, but since it's helpful to
-  know the guts of a system you are working with, here is it's definition:
-
-  @data 'ValorI' e = 'Inert' e | 'Wrong' e@
-
-  'Inert' is here because we sometimes want to inject a value that is not
-  necessarily an error (e.g. `pure` from the 'Applicative' interface). If the
-  result of validation is 'Inert' then that means there was no error.
+{- |
+  Accumulating version of 'alt' where if both operands are 'Wrong.Wrong' they
+  will be 'mappend'ed.
 -}
-newtype Valor i m e = Valor
-  { unValor :: i -> m ( ValorI e )
-  }
-
-{-|
-  The semigroup instance is used to combine multiple validators together. For
-  when you e.g. want to check that the user is both over 18 years old, and not
-  older than 45.
--}
-instance ( Monad m , Semigroup e ) => Semigroup ( Valor i m e ) where
-  Valor b <> Valor d = Valor $ \ i -> (<>) <$> (b i) <*> (d i)
-
-{-|
-  If your error value @e@ is a monoid then 'mempty' is a @'ValorI' i m e@ that
-  always passes. You can use the alias 'pass'.
--}
-instance ( Monad m , Monoid e ) => Monoid ( Valor i m e ) where
-  mempty = Valor $ const $ pure mempty
-
-{-|
-  The good old 'Functor' interface. If you want to find out if there's an error
-  before you run 'validateP' or 'validateM', instead of mapping something like
-  'Just' to get the 'Valor i m ( Maybe e )' use the 'toM' or one of the @checkM@
-  functions, as they will return 'Nothing' for when validation is 'Inert' and
-  @'Just' e@ in case of an error.
--}
-instance ( Monad m ) => Functor ( Valor i m ) where
-  fmap f ( Valor v ) = Valor $ \ i -> ( fmap f ) <$> ( v i )
-
-{-|
-  The 'Applicative' interface is important for building more complex and
-  structured error values. It is also prefered to the 'Monad' interface.
--}
-instance ( Monad m ) => Applicative ( Valor i m ) where
-  pure = Valor . const . pure . pure
-
-  ( Valor b ) <*> ( Valor d ) = Valor $ \ i -> (<*>) <$> ( b i ) <*> ( d i )
-
-{-|
-  While the 'Monad' instance is valid and quite powerful, it is recommended that
-  you use the 'Applicative' interface werever possible to avoid shooting your
-  self to the foot. While using the 'Monad' interface, __you__ are the one
-  responsible for how the errors aggregate, so be careful.
--}
-instance ( Monad m ) => Monad ( Valor i m ) where
-  b >>= bd = Valor $ \ i -> do
-    vb <- unValor b i
-    vd <- unValor ( bd $ valI vb ) i
-    pure $ case ( vb , vd ) of
-      ( Inert _ , Inert e ) -> Inert e
-      ( Inert _ , Wrong e ) -> Wrong e
-      ( Wrong _ , Inert e ) -> Wrong e
-      ( Wrong _ , Wrong e ) -> Wrong e
-
---
-
-{-|
-  Unfortunately it is not possible to create the 'Alternative' instance for
-  'Valor'. To rectify this, here's the 'alt' function which behaves like what
-  one might expect from the 'Alternative's '<|>'.
--}
-alt :: ( Monad m , Semigroup e ) => Valor i m e -> Valor i m e -> Valor i m e
-alt ( Valor b ) ( Valor d ) = Valor $ \ i -> altI <$> ( b i ) <*> ( d i )
-
---
-
-{-|
-  An alias for 'mempty'. Basically, it's a validator that always passes. If your
-  error is not a 'Monoid' then you can use 'pure' from the 'Applicative' to
-  inject a "dummy" 'Inert' value.
--}
-pass :: ( Monad m , Monoid e ) => Valor i m e
-pass = mempty
-
-{-|
-  If you want to inspect the input value before constructing a validator that
-  always passes, use this. It might come in handy when using the 'Applicative'
-  interface.
--}
-pass' :: Monad m => ( i -> m e ) -> Valor i m e
-pass' s = Valor $ fmap Inert . s
-
-{-|
-  Constructs a validator that always fails with the error @e@.
--}
-fail :: Monad m => e -> Valor i m e
-fail = Valor . const . pure . Wrong
-
-{-|
-  Inspect the input value before constructing an error to fail with.
--}
-fail' :: Monad m => ( i -> m e ) -> Valor i m e
-fail' s = Valor $ fmap Wrong . s
-
-{-|
-  Inspect the input and then succeed if the result of the inspection is
-  'Nothing' or fail with @e@ if the result is @'Just' e@.
--}
-make :: ( Monad m , Monoid e ) => ( i -> Maybe e ) -> Valor i m e
-make t = Valor $ \ i -> flip unValor i $ maybe pass fail $ t i
-
-{-|
-  Use one or the other validator depending on the result of the test.
--}
-test
-  :: Monad m
-  => Valor i m e      -- ^ validator to use if 'False'
-  -> Valor i m e      -- ^ validator to use if 'True'
-  -> ( i -> m Bool )  -- ^ function for inspecting the input
-  -> Valor i m e
-test f p t = Valor $ \ i -> t i >>= flip unValor i . bool f p
-
-{-|
-  Use one or the other validator depending on the result of the primary
-  validator. (error of the primary validator is not aggregated)
--}
-poke
-  :: Monad m
-  => Valor i m e -- ^ validator to run when primary fails
-  -> Valor i m e -- ^ validator to run when primary succeeds
-  -> Valor i m x -- ^ primary validator
-  -> Valor i m e
-poke f p t = Valor $ \ i -> do
-  vt <- unValor t i
-  flip unValor i $ case vt of
-    Wrong _ -> f
-    Inert _ -> p
-
---
-
-{-|
-  It converts the 'ValorI' value so that if the internal state is 'Inert'
-  'Nothing' is returned, and if the internal state is 'Wrong' then @'Just' e@ is
-  returned.
-
-  This can be useful in conjunction with the 'Monad' interface as it allows you
-  to know if the error has occurred during the validation process.
--}
-toM :: Monad m => Valor i m e -> Valor i m ( Maybe e )
-toM ( Valor v ) = Valor $ \ i -> do
-  vv <- v i
-  pure $ case vv of
-    Inert _ -> Inert Nothing
-    Wrong e -> Wrong $ Just e
-
-{-|
-  It modifies the 'ValorI' value so that if the internal state is 'Inert' it
-  returns the 'Valid' input value @i@ and error @e@ otherwise.
-
-  It might come in handy if you want to aggregate the inputs alongside errors.
--}
-toE :: Monad m => Valor i m e -> Valor i m ( Either ( Valid i ) e )
-toE ( Valor v ) = Valor $ \ i -> do
-  vv <- v i
-  pure $ case vv of
-    Inert _ -> Inert $ Left $ Valid i
-    Wrong e -> Wrong $ Right e
+acc :: ( Monad m , Semigroup e ) => Valor i m e -> Valor i m e -> Valor i m e
+acc ( Valor b ) ( Valor d ) = Valor $ \ i -> accW <$> ( b i ) <*> ( d i )
 
 --
 
 {- |
-  Make a validator work with another input type by providing a conversion
-  function.
+  A validator that always 'pass'es the test. Essentially just an alias for
+  'mempty'. If you want to create a validator that always passes for a type that
+  isn't a 'Monoid', then you can use 'pure', however you will have to provide it
+  a "dummy" error value that you yourself will manage as "neutral".
 -}
-check :: Monad m => ( i -> x ) -> Valor x m e -> Valor i m e
-check s v = Valor $ \ i -> unValor v ( s i )
+pass :: ( Monad m , Monoid e ) => Valor i m e
+pass = mempty
 
-{-|
-  This is useful when you want to build up a more complex and structured error
-  value where some fields can succeed and some can fail. It makes an existing
-  'ValorI' work on a value of the field of a particular input @i@.
+{- |
+  Constructs a validator that always fails with provided error @e@.
 -}
-check1
-  :: Monad m
-  => ( i -> x )   -- ^ field selector
-  -> Valor x m e  -- ^ validator
-  -> Valor i m ( Maybe e )
-check1 s = toM . check s
-
-{-|
-  Similar to 'check1', however it traverses a given validator over every item in
-  a 'Traversable' such as 'List'. If there is even a single error, it returns
-  'Just' a 'Traversable' with possible errors for each value and 'Nothing'
-  otherwise.
-
-  Very useful for validating a record field that contains a e.g. list of items.
--}
-checkN :: ( Monad m , Traversable t ) => ( i -> t x ) -> Valor x m e -> Valor i m ( Maybe ( t ( Maybe e ) ) )
-checkN s v = toM $ checkN' s ( toM v )
-  where
-    checkN' :: ( Monad m , Traversable t ) => ( i -> t x ) -> Valor x m e -> Valor i m ( t e )
-    checkN' s' v' = Valor $ \ i -> do
-      es <- traverse ( unValor v' ) ( s' i )
-      pure $ bool Wrong Inert (all isInertI es) $ fmap valI es
+fail :: Monad m => e -> Valor i m e
+fail = Valor . const . pure . Wrong
 
 --
 
-{-|
-  A "pure" validation performed within the 'Identity' monad. Use if you do not
-  have to access e.g. your database to check if the username is free or not.
+{- |
+  Apply one or the other validator depending on the result of a test.
+-}
+test
+  :: Monad m
+  => Valor i m e      -- ^ validator to use on 'False'
+  -> Valor i m e      -- ^ validator to use on 'True'
+  -> ( i -> m Bool )  -- ^ a predicate
+  -> Valor i m e
+test ( Valor f ) ( Valor p ) t = Valor $ \ i -> t i >>= bool ( f i ) ( p i )
+
+{- |
+  Construct a validator that checks the input @i@ and 'Maybe' results in an
+  error @e@.
+-}
+make :: ( Monad m , Monoid e ) => ( i -> m ( Maybe e ) ) -> Valor i m e
+make ime = Valor $ \ i -> ime i >>= flip unValor i . maybe pass fail
+
+{- |
+  Construct a validator that applies another validator depending on the result
+  from a test validator. If both the "test" and the "fail" validator fail, then
+  only the error from the "fail" validator is returned.
+-}
+peek :: ( Monad m , Semigroup e ) => Valor i m e -> Valor i m e -> Valor i m e -> Valor i m e
+peek ( Valor f ) ( Valor p ) ( Valor t ) = Valor $ \ i -> t i >>= wrong ( const $ f i ) ( const $ p i )
+
+{- |
+  Just like 'peek', except if both the "test" and the "fail" validators fail,
+  their results are 'mappend'ed ('<>').
+-}
+poke :: ( Monad m , Semigroup e ) => Valor i m e -> Valor i m e -> Valor i m e -> Valor i m e
+poke ( Valor f ) ( Valor p ) ( Valor t ) = Valor $ \ i -> do
+  tr <- t i
+  case tr of
+    Inert _ -> p i
+    Wrong b -> do
+      tr' <- f i
+      pure $ case tr' of
+        Inert _ -> Wrong b
+        Wrong d -> Wrong $ b <> d
+
+--
+
+{- |
+  If a validator fails with an error 'nerf' will make that error 'Wrong.Inert'
+  essentially making it pass.
+
+  Use of this function is discouraged, however it might come in handy in
+  combination with 'peer' within the 'Monad'ic context when you want to check
+  the result of a validation without failing the whole computation.
+
+  Be careful though, @nerf . peer@ is not the same as @peer . nerf@ (which is
+  essentially useless and will always result in 'Nothing').
+-}
+nerf :: Monad m => Valor i m e -> Valor i m e
+nerf ( Valor v ) = Valor $ \ i -> v i >>= pure . Inert . valW
+
+{- |
+  Allows you to 'peer' into the 'Wrong' contained within the 'Valor' (how
+  poetic) and if there is nothing 'Wrong.Wrong' it will return 'Nothing'.
+-}
+peer :: Monad m => Valor i m e -> Valor i m ( Maybe e )
+peer ( Valor v ) = Valor $ \ i -> v i >>= pure . wrong ( Wrong . Just ) ( const $ Inert Nothing )
+
+--
+
+{- |
+  It can 'adapt' a validator to the new input type given a conversion function,
+  making it useful for working with records (think field selectors).
+
+  This is essentially a 'Data.Functor.Contravariant.contramap' from
+  "Data.Functor.Contravariant", however, due to the placement of arguments in
+  the 'Valor' type constructor it is not possible to write that instance.
+-}
+adapt :: Monad m => ( i -> x ) -> Valor x m e -> Valor i m e
+adapt s ( Valor v ) = Valor $ v . s
+
+{- |
+  Useful for constructing structured errors / error records. By using 'Maybe'
+  you can specify for which exact field an error has occurred. It is implemented
+  using 'peer' and 'adapt'.
+-}
+check1 :: Monad m => ( i -> x ) -> Valor x m e -> Valor i m ( Maybe e )
+check1 s = peer . adapt s
+
+{- |
+  Similar to 'check1', except it will apply a validator to each element of a
+  'Traversable', e.g. a list. If every element of a list is valid, then we get
+  'Nothing', otherwise we get a list of 'Maybe's for each validated value.
+
+  This allows us to know in which exact element of a list an error has occurred
+  (if you trust your 'Traversable' to maintain the original order after the
+  traversal).
+-}
+checkN :: ( Monad m , Traversable t ) => ( i -> t x ) -> Valor x m e -> Valor i m ( Maybe ( t ( Maybe e ) ) )
+checkN s v = Valor $ \ i -> do
+  ws <- traverse ( unValor $ peer v ) ( s i )
+  pure $ if all isInert ws
+    then Inert $ Nothing
+    else Wrong $ Just $ fmap valW ws
+
+--
+
+{- |
+  Runs a validator within the 'Identity' 'Monad', essentially making it a "pure"
+  validation.
 -}
 validateP :: Valor i Identity e -> i -> Either ( Valid i ) e
-validateP ( Valor v ) i = case runIdentity ( v i ) of
-  Inert _ -> Left $ Valid i
-  Wrong e -> Right e
+validateP v = runIdentity . validateM v
 
-{-|
-  An "impure" validation performed within the user provided monad @m@. Use if
-  you have to perform some side effects during the validation process.
+{- |
+  Runs a validator within the user provided 'Monad' @m@ allowing you to perform
+  side effects during the validation, e.g. check with the application database
+  if the username is already registered.
 -}
 validateM :: Monad m => Valor i m e -> i -> m ( Either ( Valid i ) e )
-validateM ( Valor v ) i = do
-  vi <- v i
-  pure $ case vi of
-    Inert _ -> Left  $ Valid i
-    Wrong e -> Right $ e
+validateM ( Valor v ) i = v i >>= pure . wrong Right ( const $ Left $ Valid i )
